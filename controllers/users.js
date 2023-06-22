598363de78da5d8705e9521086e83e9722da88fb
ukrwebprom <ukrwebprom@gmail.com>
2023-06-22 12:30:16 +0300

    login, authenticate
    Added:
    authenticate middleware,
    /users/me route
    /users/login roure
    login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { email, password, name } = req.body;
@@ -20,4 +22,37 @@ const register = async (req, res) => {
   });
 };
 
-module.exports = { register: ctrlWrapper(register) };
+const login = async (req, res) => {
+  const {email, password} = req.body;
+  const user = await User.findOne({email});
+  if(!user) throw HttpError(404, 'Wrong email or password');
+
+  const check = await bcrypt.compare(password, user.password);
+  if(!check) throw HttpError(404, 'Wrong email or password');
+  
+  const payload = {
+      id:user._id,
+  }
+  const token = jwt.sign(payload, SECRET_KEY, {expiresIn:'24h'});
+  await User.findByIdAndUpdate(user._id, {token});
+  res.status(200).json({
+    token,
+    name: user.name
+  });
+}
+
+const me = async (req, res) => {
+  const {name, email, avatar, theme} = req.user;
+  res.status(201).json({
+    name,
+    email,
+    avatar,
+    theme
+  });
+}
+
+module.exports = { 
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  me: ctrlWrapper(me)
+ };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const ctrl = require("../../controllers/users");
 const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+router.get("/me", authenticate, ctrl.me);
 
 module.exports = router;
