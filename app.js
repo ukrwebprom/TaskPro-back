73b666d3c0ea17990f47dd6f401e6370f17c0ae7
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-06-23 23:57:10 +0500

    updates routes for user

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use("/users", usersRouter);
+app.use("/user", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
