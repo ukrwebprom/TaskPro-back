a379a2ded865ba56e42ee0b2a0d9f935a46c3a3d
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2023-06-24 21:27:50 +0300

    create helper send Support Mail

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const HandleMongooseError = require("./MongooseError");
+const sendSupportMail = require('./sendSupportMail');
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   HandleMongooseError,
+  sendSupportMail
 };
